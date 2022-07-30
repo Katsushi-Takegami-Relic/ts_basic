f521ea34ad2fff1fd92f0864be21f50d9c2dbde4
Katsushi-Takegami-Relic <katsushi.takegami@relic.co.jp>
2022-07-30 15:21:47 +0900

    type_definition_commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import World from './world'
+// import World from './world'
 
-const root = document.getElementById('root')
-const world = new World('Hello Torahack')
-world.sayHello(root)
+// const root = document.getElementById('root')
+// const world = new World('Hello Torahack')
+// world.sayHello(root)
+
+//基本の型定義
+import { primitiveSample } from './basic'
+import { notExistSample } from './basic'
+import { anySample } from './basic'
+import { unknownSample } from './basic'
+primitiveSample()
+notExistSample()
+anySample()
+unknownSample()
