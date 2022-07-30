f1f5846b770f06214a997dc3f6bbff17cf9e4948
Katsushi-Takegami-Relic <katsushi.takegami@relic.co.jp>
2022-07-30 15:23:46 +0900

    lint-fix

--- src/basic/notExist.ts
+++ src/basic/notExist.ts
@@ -1,21 +1,21 @@
 export default function notExistSample() {
-    let name = null
-    console.log('notExist sample 1:', typeof name, name)
+  let name = null
+  console.log('notExist sample 1:', typeof name, name)
 
-    name = 'かっつん'
-    if (name === null) {
-        console.log('notExist sample 2:', 'nameはnullだよ')
-    } else {
-        console.log('notExist sample 3:', 'nameはnullじゃないよ')
-    }
+  name = 'かっつん'
+  if (name === null) {
+    console.log('notExist sample 2:', 'nameはnullだよ')
+  } else {
+    console.log('notExist sample 3:', 'nameはnullじゃないよ')
+  }
 
-    let age = undefined
-    console.log('notExist sample 4:', typeof age, age)
+  let age = undefined
+  console.log('notExist sample 4:', typeof age, age)
 
-    age = 28;
-    if (age === undefined) {
-        console.log('notExist sample 5:', 'ageはundefinedだよ')
-    } else {
-        console.log('notExist sample 6:', 'ageはundefinedじゃないよ')
-    }
+  age = 28
+  if (age === undefined) {
+    console.log('notExist sample 5:', 'ageはundefinedだよ')
+  } else {
+    console.log('notExist sample 6:', 'ageはundefinedじゃないよ')
+  }
 }
--- src/basic/primitive.ts
+++ src/basic/primitive.ts
@@ -1,14 +1,14 @@
 //ただ一つだけエクスポートするものがある
 export default function primitiveSample() {
   //string
-  let name = 'かっつん' //name = 123
+  const name = 'かっつん' //name = 123
   console.log('primitive sample 1:', typeof name, name)
 
   //number
-  let age: number = 25 // age = '25'
+  const age = 25 // age = '25'
   console.log('primitive sample 2:', typeof age, age)
 
-  let isSingle: boolean = true //isSingle = 'true'
+  const isSingle = true //isSingle = 'true'
   console.log('primitive sample 3:', typeof isSingle, isSingle)
 
   const isOver20: boolean = age >= 20
